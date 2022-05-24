07c07fe390cc1d13b91d1184930cb5477e9e1044
humminghorse <r.imahashi0724@gmail.com>
2022-05-24 11:50:07 +0900

    フィードバックいただいた内容を反映

--- components/CoordinationImage.vue
+++ components/CoordinationImage.vue
@@ -4,13 +4,15 @@
     <img class="object-cover h-600 w-full" :src='coordinationImageUrl()'>
     <!-- 参考: 【HTML・CSS】画像の上に画像や文字を重ねる方法 https://public-constructor.com/display-icon-on-image/ -->
     <!-- 参考: CSSのposition: absoluteとrelativeとは https://uxmilk.jp/63409 -->
-    <div class="w-20 h-6 pd-2 bg-black bg-opacity-50 absolute bottom-1 left-1">
+    <div class="w-20 h-6 bg-black bg-opacity-50 absolute bottom-1 left-1">
       <div class="w-full h-full flex items-center justify-center gap-1 text-white text-xs">
-        <EyeIcon class="w-4"></EyeIcon>
+        <EyeIcon class="w-4 h-4 shrink-0"></EyeIcon>
         <p>{{watchedCount}}</p>
       </div>
     </div>
     <!-- アイテムタグ -->
+    <!-- TODO: daisyのtooltipを使う https://daisyui.com/components/tooltip/ -->
+    <!-- TODO: https://daisyui.com/components/button/#icon-at-end -->
     <a
       class="w-24 h-6 pd-2 absolute flex items-center justify-center bg-black bg-opacity-75 text-white text-xs"
       :style='itemTagPosition'
