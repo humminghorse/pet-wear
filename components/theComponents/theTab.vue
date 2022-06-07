9adf3c8ff9f4f99c302d378a6377518f92239e81
humminghorse <r.imahashi0724@gmail.com>
2022-06-07 10:16:38 +0900

    タブのアクティブクラスを動的に設定する

--- components/theComponents/theTab.vue
+++ components/theComponents/theTab.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="tabs tabs-boxed w-full">
-    <!-- TODO: tab-activeを動的に設定したい -->
-    <a href="/coordinates" class="tab w-1/4 tab-active">コーディネート</a>
-    <a href="/pets" class="tab w-1/4">ペット</a>
-    <a href="/items" class="tab w-1/4">アイテム</a>
-    <a href="/shops" class="tab w-1/4">ショップ</a>
+    <!-- TODO: tabとw-1/4をclassにしたい -->
+    <a href="/coordinates" :class="['tab', 'w-1/4', {'tab-active': isCoordinates}]">コーディネート</a>
+    <a href="/pets" :class="['tab', 'w-1/4', {'tab-active': isPets}]">ペット</a>
+    <a href="/items" :class="['tab', 'w-1/4', {'tab-active': isItems}]">アイテム</a>
+    <a href="/shops" :class="['tab', 'w-1/4', {'tab-active': isShops}]">ショップ</a>
   </div>
 </template>
 <script setup lang="ts">
-
+const route = useRoute()
+const isCoordinates = computed((): boolean => {
+  return route.path === '/coordinates'
+})
+const isPets = computed((): boolean => {
+  return route.path === '/pets'
+})
+const isItems = computed((): boolean => {
+  return route.path === '/items'
+})
+const isShops = computed((): boolean => {
+  return route.path === '/shops'
+})
 </script>
