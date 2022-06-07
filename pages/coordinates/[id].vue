f7559a0dff0df5650ad3cdc2b760f320ce60aa27
humminghorse <r.imahashi0724@gmail.com>
2022-06-07 09:20:22 +0900

    全ページ共通の部分をlayoutsに移設する

--- pages/coordinates/[id].vue
+++ pages/coordinates/[id].vue
@@ -1,94 +1,88 @@
 <template>
-  <div class="container mx-auto pl-4 pr-4 bg-white">
-    <TheHeader />
+  <!-- パンくずリストとシェアボタン -->
+  <div class="flex justify-between">
+    <div class="text-sm breadcrumbs">
+      <ul>
+        <li><a href="/">TOP</a></li>
+        <!-- <li><a>今橋 陵</a></li>  -->
+        <li><a>{{coordinateDetail.petName}}</a></li>
+        <li>{{yearMonthDate(coordinateDetail.date)}}のコーディネート</li>
+      </ul>
+    </div>
+    <div class="mt-2">
+      <button class="btn w-24 bg-accent text-white min-h-0 h-full flex p-2">
+        <ShareIcon class="w-5 mr-0.5 shrink-0"/>
+        <span>シェアする</span>
+      </button>
+    </div>
+  </div>
 
-    <!-- パンくずリストとシェアボタン -->
-    <div class="flex justify-between">
-      <div class="text-sm breadcrumbs">
-        <ul>
-          <li><a href="/">TOP</a></li>
-          <!-- <li><a>今橋 陵</a></li>  -->
-          <li><a>{{coordinateDetail.petName}}</a></li>
-          <li>{{yearMonthDate(coordinateDetail.date)}}のコーディネート</li>
-        </ul>
+  <!-- ユーザー別のヘッダー -->
+  <div class="flex justify-between items-center w-full my-4">
+    <div class="flex items-center">
+      <div class="h-16 w-16">
+        <img class="object-cover h-full w-full rounded-full" :src="coordinateDetail.petIconSrc">
       </div>
-      <div class="mt-2">
-        <button class="btn w-24 bg-accent text-white min-h-0 h-full flex p-2">
-          <ShareIcon class="w-5 mr-0.5 shrink-0"/>
-          <span>シェアする</span>
-        </button>
+      <div class="ml-4">
+        <p class="text-2xl">{{coordinateDetail.petName}}</p>
       </div>
     </div>
-
-    <!-- ユーザー別のヘッダー -->
-    <div class="flex justify-between items-center w-full my-4">
-      <div class="flex items-center">
-        <div class="h-16 w-16">
-          <img class="object-cover h-full w-full rounded-full" :src="coordinateDetail.petIconSrc">
-        </div>
-        <div class="ml-4">
-          <p class="text-2xl">{{coordinateDetail.petName}}</p>
-        </div>
-      </div>
-      <div class="pt-2">
-        <button class="btn gap-x-0.5 min-h-0 h-full flex p-2 bg-accent text-white">
-          <UserAddIcon class="w-5"/>
-          <span class="text-white">フォローする</span>
-        </button>
-      </div>
+    <div class="pt-2">
+      <button class="btn gap-x-0.5 min-h-0 h-full flex p-2 bg-accent text-white">
+        <UserAddIcon class="w-5"/>
+        <span class="text-white">フォローする</span>
+      </button>
     </div>
+  </div>
 
-    <!-- ボディ -->
-    <div class="flex items-start bg-neutral">
-      <!-- サブコンテンツ -->
-      <div class="w-3/5">
-        <CoordinateImage
-          :coordinateImgSrc="coordinateDetail.coordinateImgSrc"
-          :watchedCount="coordinateDetail.watchedCount"
-          :itemTagPosition="coordinateDetail.itemTagPosition"
-          :itemName="coordinateDetail.itemName"
-        />
-        
-        <!-- TODO: 各種アクションボタン -->
-        <div>
-
-        </div>
-        <!-- TODO: コメント欄 -->
-        <div>
-
-        </div>
+  <!-- ボディ -->
+  <div class="flex items-start bg-neutral">
+    <!-- サブコンテンツ -->
+    <div class="w-3/5">
+      <CoordinateImage
+        :coordinateImgSrc="coordinateDetail.coordinateImgSrc"
+        :watchedCount="coordinateDetail.watchedCount"
+        :itemTagPosition="coordinateDetail.itemTagPosition"
+        :itemName="coordinateDetail.itemName"
+      />
+      
+      <!-- TODO: 各種アクションボタン -->
+      <div>
 
       </div>
-      <!-- メインコンテンツ -->
-      <div class="w-2/5 p-4">
-        <CoordinateDetail
-          :petName="coordinateDetail.petName"
-          :itemName="coordinateDetail.itemName"
-          :description="coordinateDetail.description"
-          :date="coordinateDetail.date"
-        />
-        <div class="w-full mt-4 px-4 py-4 bg-white border-2">
-          <h1 class="text-lg">着用アイテム</h1>
-          <div class="flex gap-2 items-center justify-center">
-            <div class="flex flex-col items-center">
-              <figure class="pt-2">
-                <img
-                  :src="coordinateDetail.itemImgSrc"
-                  class="object-cover w-64 rounded-xl"
-                />
-              </figure>
-              <p class="my-2 py-2">{{coordinateDetail.itemName}}</p>
-            </div>
+      <!-- TODO: コメント欄 -->
+      <div>
+
+      </div>
+
+    </div>
+    <!-- メインコンテンツ -->
+    <div class="w-2/5 p-4">
+      <CoordinateDetail
+        :petName="coordinateDetail.petName"
+        :itemName="coordinateDetail.itemName"
+        :description="coordinateDetail.description"
+        :date="coordinateDetail.date"
+      />
+      <div class="w-full mt-4 px-4 py-4 bg-white border-2">
+        <h1 class="text-lg">着用アイテム</h1>
+        <div class="flex gap-2 items-center justify-center">
+          <div class="flex flex-col items-center">
+            <figure class="pt-2">
+              <img
+                :src="coordinateDetail.itemImgSrc"
+                class="object-cover w-64 rounded-xl"
+              />
+            </figure>
+            <p class="my-2 py-2">{{coordinateDetail.itemName}}</p>
           </div>
         </div>
-        <!-- TODO: タグ -->
-        <div>
-          
-        </div>
+      </div>
+      <!-- TODO: タグ -->
+      <div>
+        
       </div>
     </div>
-
-    <TheFooter />
   </div>
 </template>
 <script setup lang="ts">
--- pages/coordinates/index.vue
+++ pages/coordinates/index.vue
@@ -1,57 +1,52 @@
 <template>
-  <div class="container mx-auto pl-4 pr-4 bg-white">
-    <TheHeader />
-    <div class="tabs tabs-boxed w-full">
-      <a class="tab w-1/4 tab-active">コーディネート</a>
-      <a class="tab w-1/4">ペット</a>
-      <a class="tab w-1/4">アイテム</a>
-      <a class="tab w-1/4">ショップ</a>
-    </div>
-    <div class="border-b-2">
-      <h1 class="mt-4 text-2xl">コーディネート一覧</h1>
-      <p class="mt-2 text-right">{{FilteredCoordinateList.length}}件</p>
-    </div>
+  <div class="tabs tabs-boxed w-full">
+    <a class="tab w-1/4 tab-active">コーディネート</a>
+    <a class="tab w-1/4">ペット</a>
+    <a class="tab w-1/4">アイテム</a>
+    <a class="tab w-1/4">ショップ</a>
+  </div>
+  <div class="border-b-2">
+    <h1 class="mt-4 text-2xl">コーディネート一覧</h1>
+    <p class="mt-2 text-right">{{FilteredCoordinateList.length}}件</p>
+  </div>
 
-    <!-- body -->
-    <div class="flex items-start bg-neutral">
-      <div class="control-panel bg-white flex-auto m-4 p-2 w-1/12">
-        <!-- <BaseRadio
-          radioTitle="ペットの種類"
-          radioName="radio-pet-type"
-          v-bind:labelTexts="['犬', '猫']"
-        />
-        <BaseRadio
-          radioTitle="ペットの色"
-          radioName="radio-pet-color"
-          v-bind:labelTexts="['白', '黒', '茶']"
-        />
-        <BaseRadio
-          radioTitle="アイテムの種類"
-          radioName="radio-item-type"
-          v-bind:labelTexts="['首輪', '服', 'その他']"
-        /> -->
-        <BaseRadio
-          radioTitle="アイテムの色"
-          radioName="radio-item-color"
-          :radioItems="itemColors"
-          :checkedValue="checkedItemColor"
-          @update:checkedValue='newValue => {checkedItemColor = newValue}'
-        />
-      </div>
-      <div class="content flex flex-wrap gap-4 mx-4 my-4 w-3/4">
-        <template v-for="coordinateListItem in  FilteredCoordinateList" :key="coordinateListItem.coordinateId">
-          <CoordinateListItem
-            :coordinateId="coordinateListItem.coordinateId"
-            :coordinateImgSrc="coordinateListItem.coordinateImgSrc"
-            :itemImgSrc="coordinateListItem.itemImgSrc"
-            :itemName="coordinateListItem.itemName"
-            itemColor=""
-          />
-        </template>
-      </div>
+  <!-- body -->
+  <div class="flex items-start bg-neutral">
+    <div class="control-panel bg-white flex-auto m-4 p-2 w-1/12">
+      <!-- <BaseRadio
+        radioTitle="ペットの種類"
+        radioName="radio-pet-type"
+        v-bind:labelTexts="['犬', '猫']"
+      />
+      <BaseRadio
+        radioTitle="ペットの色"
+        radioName="radio-pet-color"
+        v-bind:labelTexts="['白', '黒', '茶']"
+      />
+      <BaseRadio
+        radioTitle="アイテムの種類"
+        radioName="radio-item-type"
+        v-bind:labelTexts="['首輪', '服', 'その他']"
+      /> -->
+      <BaseRadio
+        radioTitle="アイテムの色"
+        radioName="radio-item-color"
+        :radioItems="itemColors"
+        :checkedValue="checkedItemColor"
+        @update:checkedValue='newValue => {checkedItemColor = newValue}'
+      />
     </div>
-
-    <TheFooter />
+    <div class="content flex flex-wrap gap-4 mx-4 my-4 w-3/4">
+      <template v-for="coordinateListItem in  FilteredCoordinateList" :key="coordinateListItem.coordinateId">
+        <CoordinateListItem
+          :coordinateId="coordinateListItem.coordinateId"
+          :coordinateImgSrc="coordinateListItem.coordinateImgSrc"
+          :itemImgSrc="coordinateListItem.itemImgSrc"
+          :itemName="coordinateListItem.itemName"
+          itemColor=""
+        />
+      </template>
+    </div>
   </div>
 </template>
 <script setup lang="ts"> // 参考: Nuxt3におけるscript setupの基本的な使い方とFAQ(https://zenn.dev/coedo/articles/86bc31acb4ea47)
