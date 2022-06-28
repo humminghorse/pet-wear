e7148c9f010f73c190e98a1d2c6306328c67f3cc
humminghorse <r.imahashi0724@gmail.com>
2022-06-28 09:58:20 +0900

    ヘッダーをレスポンシブにする

--- components/theComponents/theHeader.vue
+++ components/theComponents/theHeader.vue
@@ -3,10 +3,11 @@
     <div class="navbar-start">
     </div>
     <div class="navbar-center">
+      <!-- TODO: デバイスが小さい時にP E T W E A Rを真ん中に配置したい -->
       <a href="/" class="btn btn-ghost normal-case text-xl">P E T W E A R</a>
     </div>
-    <div class="navbar-end">
-      <button class="btn btn-ghost">
+    <div class="navbar-end w-1/5 sm:w-1/2">
+      <button class="btn btn-ghost px-1 sm:px-4">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           class="h-6 w-6"
@@ -21,9 +22,9 @@
             d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
           />
         </svg>
-        <span class="ml-2">今橋 陵さん</span>
+        <span class="hidden sm:inline-flex ml-2">今橋 陵さん</span>
       </button>
-      <button class="btn btn-ghost btn-circle">
+      <button class="btn btn-ghost btn-circle w-8 sm:w-12">
         <div class="indicator">
           <svg
             xmlns="http://www.w3.org/2000/svg"
