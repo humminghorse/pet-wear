17fa1b254103b6bf649870824b28effebdb0fe64
humminghorse <r.imahashi0724@gmail.com>
2022-07-26 09:58:41 +0900

    アカウント作成フォームを追加

--- pages/signup/index.vue
+++ pages/signup/index.vue
@@ -20,7 +20,23 @@
   </div>
   
   <h2 class="mt-4 text-xl">まずはあなたの情報を入力してください。</h2>
-
+  <div class="mt-4">
+    <!-- TODO: バリデーションとエラーメッセージ -->
+    <form class="flex flex-col gap-y-2">
+      <div>
+        <label for="name">名前</label>
+        <input id="name" type="text" placeholder="鈴木一郎" class="ml-2 border" />
+      </div>
+      <div>
+        <label for="email">メールアドレス</label>
+        <input id="email" type="email" placeholder="xxx@example.com" class="ml-2 border" />
+      </div>
+      <div>
+        <label for="password">パスワード</label>
+        <input id="password" type="password" placeholder="xxxxxxxx" class="ml-2 border" />
+      </div>
+    </form>
+  </div>
 </template>
 <script setup lang="ts">
 </script>
