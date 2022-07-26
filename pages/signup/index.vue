7d5c9791b659555a31aa868c9370521e7b1d3c4f
humminghorse <r.imahashi0724@gmail.com>
2022-07-26 09:02:28 +0900

    会員登録ページを追加

---
+++ pages/signup/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- タイトル -->
+  <h1 class="mt-4 text-2xl ml-2 sm:ml-0">会員登録</h1>
+
+  <!-- body -->
+  <div class="mt-4 flex justify-center">
+    <ul class="steps">
+      <li class="step step-primary">アカウント作成</li>
+      <li class="step">ペット登録</li>
+      <li class="step">利用開始</li>
+    </ul>
+  </div>
+  
+  <h2 class="mt-4 text-xl">まずはあなたの情報を入力してください。</h2>
+
+</template>
+<script setup lang="ts">
+</script>
