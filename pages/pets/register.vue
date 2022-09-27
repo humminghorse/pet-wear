ddec9c9df1b72a12e0ded752cabe24ab6ea3a053
humminghorse <r.imahashi0724@gmail.com>
2022-09-27 15:04:34 +0900

    調整

--- pages/pets/register.vue
+++ pages/pets/register.vue
@@ -5,100 +5,104 @@
       <h1 class="mt-4 text-2xl ml-2 sm:ml-0">ペット登録</h1>
       <!-- body -->
       <form @submit.prevent class="mx-8 sm:mt-4">
-        <!-- 名前 -->
-        <div class="form-item">
-          <label for="name" class="label sm:w-1/3">名前</label>
-          <input id="name" v-model="store.name" type="text" class="input input-bordered w-full"/>
-        </div>
-        <!-- アイコン画像 -->
-        <div class="form-item">
-          <label for="iconFiles" class="label sm:w-1/3">アイコン画像</label>
-          <input id="iconFiles" @change="uploadIconFile" type="file" class="w-full" />
-        </div>
-        <!-- 紹介文 -->
-        <div class="form-item">
-          <label for="introduction" class="label sm:w-1/3">紹介文</label>
-          <textarea id="introduction" v-model="store.introduction" type="text" class="textarea textarea-bordered w-full"></textarea>
-        </div>
-        <!-- 分類 -->
-        <div class="form-item">
-          <legend for="classification" class="label sm:w-1/3">分類</legend>
-          <div class="flex gap-4">
-            <div class="form-control">
-              <label class="label sm:w-1/3 cursor-pointer justify-start gap-2">
-                <input type="radio" id="dog" name="classification" value="dog" class="radio" v-model="store.classification"/>
-                <span class="label sm:w-1/3-text">犬</span> 
-              </label>
-            </div>
-            <div class="form-control">
-              <label class="label sm:w-1/3 cursor-pointer justify-start gap-2">
-                <input type="radio" id="cat" name="classification" value="cat" class="radio" v-model="store.classification"/>
-                <span class="label sm:w-1/3-text">猫</span> 
-              </label>
+        <div>
+          <!-- 名前 -->
+          <div class="form-item">
+            <label for="name" class="label sm:w-1/4">名前</label>
+            <input id="name" v-model="store.name" type="text" class="input input-bordered w-full sm:w-3/4"/>
+          </div>
+          <!-- アイコン画像 -->
+          <div class="form-item">
+            <label for="iconFiles" class="label sm:w-1/4">アイコン画像</label>
+            <input id="iconFiles" @change="uploadIconFile" type="file" class="w-full sm:w-3/4" />
+          </div>
+          <!-- 紹介文 -->
+          <div class="form-item">
+            <label for="introduction" class="label sm:w-1/4">紹介文</label>
+            <textarea id="introduction" v-model="store.introduction" type="text" class="textarea textarea-bordered w-full sm:w-3/4"></textarea>
+          </div>
+          <!-- 分類 -->
+          <div class="form-item">
+            <legend for="classification" class="label sm:w-1/4">分類</legend>
+            <div class="flex gap-4">
+              <div class="form-control">
+                <label class="label cursor-pointer justify-start gap-2">
+                  <input type="radio" id="dog" name="classification" value="dog" class="radio" v-model="store.classification"/>
+                  <span class="label sm:w-1/3-text">犬</span> 
+                </label>
+              </div>
+              <div class="form-control">
+                <label class="label cursor-pointer justify-start gap-2">
+                  <input type="radio" id="cat" name="classification" value="cat" class="radio" v-model="store.classification"/>
+                  <span class="label">猫</span> 
+                </label>
+              </div>
             </div>
           </div>
-        </div>
-        <!-- 犬種/猫種 -->
-        <div class="form-item">
-          <label for="breed" class="label sm:w-1/3">猫種</label>
-          <select v-model="store.breedId" class="select select-bordered w-full sm:w-1/3">
-            <option disabled value="">選択してください</option>
-            <option value="1">マンチカン</option>
-            <option value="2">ノルウェージャンフォレストキャット</option>
-            <option value="3">雑種</option>
-          </select>
-        </div>
-        <!-- 誕生日 -->
-        <div class="form-item">
-          <label for="birthday" class="label sm:w-1/3">誕生日</label>
-          <!-- OPTIMIZE: borderを他のinputと統一したい(色、線の太さ、radiusなど) -->
-          <input id="birthday" v-model="store.birthday" type="date" class="input input-bordered w-full"/>
-        </div>
-        <!-- 性別 -->
-        <div class="form-item">
-          <legend for="sex" class="label sm:w-1/3">性別</legend>
-          <div class="flex gap-4">
-            <div class="form-control">
-              <label class="label sm:w-1/3 cursor-pointer justify-start gap-2">
-                <input type="radio" id="male" name="sex" value="male" class="radio checked:bg-blue-500" v-model="store.sex"/>
-                <span class="label sm:w-1/3-text">オス</span> 
-              </label>
-            </div>
-            <div class="form-control">
-              <label class="label sm:w-1/3 cursor-pointer justify-start gap-2">
-                <input type="radio" id="female" name="sex" value="female" class="radio checked:bg-red-500" v-model="store.sex"/>
-                <span class="label sm:w-1/3-text">メス</span> 
-              </label>
+          <!-- 犬種/猫種 -->
+          <div class="form-item">
+            <label for="breed" class="label sm:w-1/4">猫種</label>
+            <select v-model="store.breedId" class="select select-bordered w-full sm:w-3/4">
+              <option disabled value="">選択してください</option>
+              <option value="1">マンチカン</option>
+              <option value="2">ノルウェージャンフォレストキャット</option>
+              <option value="3">雑種</option>
+            </select>
+          </div>
+          <!-- 誕生日 -->
+          <div class="form-item">
+            <label for="birthday" class="label sm:w-1/4">誕生日</label>
+            <!-- OPTIMIZE: borderを他のinputと統一したい(色、線の太さ、radiusなど) -->
+            <input id="birthday" v-model="store.birthday" type="date" class="input input-bordered w-full sm:w-3/4"/>
+          </div>
+          <!-- 性別 -->
+          <div class="form-item">
+            <legend for="sex" class="label sm:w-1/4">性別</legend>
+            <div class="flex gap-4">
+              <div class="form-control">
+                <label class="label cursor-pointer justify-start gap-2">
+                  <input type="radio" id="male" name="sex" value="male" class="radio checked:bg-blue-500" v-model="store.sex"/>
+                  <span class="label">オス</span> 
+                </label>
+              </div>
+              <div class="form-control">
+                <label class="label cursor-pointer justify-start gap-2">
+                  <input type="radio" id="female" name="sex" value="female" class="radio checked:bg-red-500" v-model="store.sex"/>
+                  <span class="label">メス</span> 
+                </label>
+              </div>
             </div>
           </div>
-        </div>
-        <!-- 体重 -->
-        <div class="form-item">
-          <label for="weightKg" class="label sm:w-1/3">体重(kg)</label>
-          <input id="weightKg" v-model="store.weightKg" type="input-tel" class="input input-bordered w-full"/>
-        </div>
-        <!-- 毛の色 -->
-        <div class="form-item">
-          <legend for="furColor" class="label sm:w-1/3">毛の色</legend>
-          <span class="ml-1 text-xs">当てはまるものを複数選んでください</span>
-          <div class="flex gap-4">
-            <div class="form-control">
-              <label class="label sm:w-1/3 cursor-pointer justify-start gap-2">
-                <input type="checkbox" checked="checked" id="black" name="furColor" value="black" v-model="store.furColor" class="checkbox" />
-                <span class="label sm:w-1/3-text">黒</span>
-              </label>
-            </div>
-            <div class="form-control">
-              <label class="label sm:w-1/3 cursor-pointer justify-start gap-2">
-                <input type="checkbox" checked="checked" id="white" name="furColor" value="white" v-model="store.furColor" class="checkbox" />
-                <span class="label sm:w-1/3-text">白</span>
-              </label>
-            </div>
-            <div class="form-control">
-              <label class="label sm:w-1/3 cursor-pointer justify-start gap-2">
-                <input type="checkbox" checked="checked" id="brown" name="furColor" value="brown" v-model="store.furColor" class="checkbox" />
-                <span class="label sm:w-1/3-text">茶</span>
-              </label>
+          <!-- 体重 -->
+          <div class="form-item">
+            <label for="weightKg" class="label sm:w-1/4">体重(kg)</label>
+            <input id="weightKg" v-model="store.weightKg" type="input-tel" class="input input-bordered w-full sm:w-1/4"/>
+          </div>
+          <!-- 毛の色 -->
+          <div class="form-item">
+            <legend for="furColor" class="label sm:w-1/4">毛の色</legend>
+            <div>
+              <span class="ml-1 text-xs">当てはまるものを複数選んでください</span>
+              <div class="flex gap-4">
+                <div class="form-control">
+                  <label class="label cursor-pointer justify-start gap-2">
+                    <input type="checkbox" checked="checked" id="black" name="furColor" value="black" v-model="store.furColor" class="checkbox" />
+                    <span class="label">黒</span>
+                  </label>
+                </div>
+                <div class="form-control">
+                  <label class="label cursor-pointer justify-start gap-2">
+                    <input type="checkbox" checked="checked" id="white" name="furColor" value="white" v-model="store.furColor" class="checkbox" />
+                    <span class="label">白</span>
+                  </label>
+                </div>
+                <div class="form-control">
+                  <label class="label cursor-pointer justify-start gap-2">
+                    <input type="checkbox" checked="checked" id="brown" name="furColor" value="brown" v-model="store.furColor" class="checkbox" />
+                    <span class="label">茶</span>
+                  </label>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -158,6 +162,8 @@ const registerPet = () => {
 </script>
 <style lang="sass">
   .form-item
-    @apply sm:flex sm:p-4 sm:border sm:items-center
+    @apply sm:flex sm:p-4 sm:border-t sm:items-center
+    &:last-child
+      @apply sm:border-b
 
 </style>
